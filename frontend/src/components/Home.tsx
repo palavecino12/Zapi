2862724cdfa9f1d64a8a0b9c8d1c6308be3b0606
Facundo <facundopalavecino054@gmail.com>
2026-04-21 20:45:26 -0300

    mergeo de home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,11 +1,11 @@
 const Home = () => {
-    return (
+  return (
     <div className="w-full h-[100dvh] bg-gradient-to-b from-orange-400 to-orange-600 flex items-center justify-center">
-        <img
+      <img
         src="/logo.png"
         className="w-32 h-32 object-contain"
-       />
-     </div>
+      />
+    </div>
   );
 };
 
