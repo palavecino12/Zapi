2a03728268b00b567e80e4b9bdf50db8ac881ae5
Facundo <facundopalavecino054@gmail.com>
2026-04-23 20:52:01 -0300

    elimine los componentes del archivo app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,4 @@
-import { CameraView } from './scanner/CameraView'
-import  Cartitem  from './cart/CartItem';
-
 function App() {
-  return (
-    <>
-      <Cartitem nombre="Zapatillas" precio={15000} />
-      <CameraView />
-    </>
-  );
 }
 
 export default App
