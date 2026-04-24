1d908f30b1bf6ca745508ac1b2811b0475be729e
maurososa8051 <maurososa8051@gmail.com>
2026-04-24 16:53:08 -0300

    Importacion del componente button al componente ViewHome

---
+++ frontend/src/pages/client/ViewHome.tsx
@@ -0,0 +1,20 @@
+import Button from "../../components/Button";
+
+const ViewHome = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Button>
+        Añadir producto
+      </Button>
+    </div>
+  );
+};
+
+export default ViewHome;
