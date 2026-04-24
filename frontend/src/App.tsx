0080af62b876a6d5021f57687b0d47ce4c24e591
Facundo <facundopalavecino054@gmail.com>
2026-04-23 21:09:35 -0300

    elimine el componente importado en app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,4 @@
-import { CameraView } from './scanner/CameraView'
-import ProductDetails from './pages/client/components/ProductDetails'
-
 function App() {
-  return (
-    <>
-      <ProductDetails nombre="Zapatillas" precio={95000} />
-      <CameraView />
-    </>
-  );
 }
 
 export default App
