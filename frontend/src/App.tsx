e92a78b897a7a3b81534cd25ce379d74fb65096c
Facundo <facundopalavecino054@gmail.com>
2026-04-23 21:16:29 -0300

    Elimine le componenten de app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,4 @@
-//import { CameraView } from './scanner/CameraView'
-import { ScanInstruction } from "./components/ScanInstruction"
-
 function App() {
-  return <ScanInstruction/>
-  //return <CameraView/>
 }
 
 export default App
