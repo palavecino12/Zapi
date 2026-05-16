c55d2ca6ad61fc5d3ebcdaa26fadd82bd12b7e69
Facundo <facundopalavecino054@gmail.com>
2026-05-16 16:12:23 -0300

    variables de entorno del front

--- frontend/src/services/api.service.ts
+++ frontend/src/services/api.service.ts
@@ -1,6 +1,11 @@
 //Servicios encargados de comunicarse con la API del backend
 
 import type { Product } from "../types/product.types"
+const apiUrl = import.meta.env.VITE_API_URL
+if (apiUrl) {
+    console.error("La variable de entorno VITE_API_URL no está definida");
+}
+
 
 //type de lo que retorna el back al momento de eliminar un ususario con exito
 type DeleteProductResponse = {
@@ -11,13 +16,13 @@ type DeleteProductResponse = {
 //service para traer un producto segun su codigo
 export const getProductByCode = async (code: string): Promise<Product> => {
     try {
-        const url = `http://localhost:3000/products/${code}`
+        const url = `${apiUrl}/products/${code}`
 
         const response = await fetch(url)
 
         if (!response.ok) {
             const errorResponse = await response.json()
-            throw new Error(errorResponse.message)//retornamos el mensaje de error que creo el back
+            throw new Error(errorResponse.message || "Error desconocido al obtener el producto")//retornamos el mensaje de error que creo el back
         }
 
         const product: Product = await response.json();
@@ -32,7 +37,7 @@ export const getProductByCode = async (code: string): Promise<Product> => {
 //Service para eliminar un producto segun su codigo
 export const deleteProductByCode = async (code: string): Promise<DeleteProductResponse> => {
     try {
-        const url = `http://localhost:3000/products/${code}`
+        const url = `${apiUrl}/products/${code}`
 
         const response = await fetch(url, {
             method: "DELETE"
@@ -40,7 +45,7 @@ export const deleteProductByCode = async (code: string): Promise<DeleteProductRe
 
         if (!response.ok) {
             const errorResponse = await response.json()
-            throw new Error(errorResponse.message)
+            throw new Error(errorResponse.message || "Error desconocido al eliminar el producto")
         }
 
         const data: DeleteProductResponse = await response.json();
