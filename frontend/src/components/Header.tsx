98d337630db2f767118c8ee9e5f13c65e5bf7412
facufornes <exequielfornes@gmail.com>
2026-07-13 00:00:40 -0300

    Cambio en CartItem, Header y Cart List

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,8 +4,8 @@ type Props = {
 
 function Header({ title = "Mi carrito" }: Props) {
     return (
-        <div className="w-full bg-violet-600 px-4 py-3 mb-3 shadow-lg">
-            <h2 className="text-white text-lg font-semibold flex center-items justify-center">
+        <div className="w-full bg-white px-4 py-3 mb-3 shadow-lg">
+            <h2 className="text-violet-600 text-lg font-bold flex center-items justify-center">
                 {title}
             </h2>
         </div>
