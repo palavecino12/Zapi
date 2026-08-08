4790d18c214b38876efb8153d081c314027e5f1f
Facundo <facundopalavecino054@gmail.com>
2026-08-07 21:20:41 -0300

    Prueba de API de mercado pago para confurmar el pago

--- backend/src/modules/payment/controller.ts
+++ backend/src/modules/payment/controller.ts
@@ -4,7 +4,7 @@ import * as paymentService from "./service";
 export const Webhook = async (req: Request, res: Response) => {
     console.log("Webhook recibido");
     console.dir(req.body, { depth: null });
-    
+
     const paymentId = req.body.data?.id;
 
     if (!paymentId) {
@@ -13,6 +13,10 @@ export const Webhook = async (req: Request, res: Response) => {
 
     const payment = await paymentService.getPayment(paymentId);
     console.dir(payment, { depth: null });
+    
+    console.log("STATUS:", payment.status);
+    console.log("SALE ID:", payment.external_reference);
+    console.log("AMOUNT:", payment.transaction_amount);
 
     return res.sendStatus(200);
 }
